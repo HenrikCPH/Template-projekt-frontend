407fe643d7f7b56c1837753a7b2df502d9eeb312
Henrik <Henne3660@hotmail.com>
2020-11-13 14:02:12 +0100

    Done

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
 export const localURL = "http://localhost:8080/jpareststarter";
-export const dropletURL = "https://hulabulakongen.dk/";
+export const dropletURL = "https://hulabulakongen.dk/CA3";
 
